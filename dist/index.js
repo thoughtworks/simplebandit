e4f95e20b60954adb864c2ac7fdd3e188209e456
Oege Dijk <oege.dijk@fourkind.com>
2023-11-03 13:09:37 +0100

    update examples with object constructor

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const { SimpleOracle, SimpleBandit, MultiBandit } = require("./src");
-window.SimpleOracle = SimpleOracle;
-window.SimpleBandit = SimpleBandit;
-window.MultiBandit = MultiBandit;
-module.exports = { SimpleOracle, SimpleBandit, MultiBandit };
