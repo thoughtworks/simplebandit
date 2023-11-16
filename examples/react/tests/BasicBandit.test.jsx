3189864367b24e7b4c8519cecb4170a1e9a65740
Oege Dijk <oege.dijk@fourkind.com>
2023-11-16 22:01:08 +0100

    fixes example test

--- examples/react/tests/BasicBandit.test.jsx
+++ examples/react/tests/BasicBandit.test.jsx
@@ -1,51 +1,66 @@
-import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
-import BasicFruitBandit from '../BasicBandit';
+import React from "react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import BasicFruitBandit from "../BasicBandit";
 import { SimpleBandit } from "../../../dist/index";
-  
-jest.mock('../../../dist/index', () => {
+
+jest.mock("../../../dist/index", () => {
   return {
-    SimpleBandit: jest.fn().mockImplementation(() => {
+    SimpleBandit: jest.fn().mockImplementation(function () {
+      this.accept = jest.fn().mockResolvedValue([]);
+      this.reject = jest.fn().mockResolvedValue([]);
+
       return {
-        getScoredActions: () => [{ actionId: 'apple', score:0.5,  probability: 0.33 }, { actionId: 'pear', score: 0.5, probability: 0.33 }, { actionId: 'orange', score:0.5, probability: 0.33 }],
-        recommend: () => ({ context: {}, actionId: 'apple', score: 0.5, probability: 0.33}),
-        accept: jest.fn(),
-        reject: jest.fn(),
+        getScoredActions: () => [
+          { actionId: "apple", score: 0.5, probability: 0.33 },
+          { actionId: "pear", score: 0.5, probability: 0.33 },
+          { actionId: "orange", score: 0.5, probability: 0.33 },
+        ],
+        recommend: () => ({
+          context: {},
+          actionId: "apple",
+          score: 0.5,
+          probability: 0.33,
+        }),
+        accept: this.accept,
+        reject: this.reject,
       };
     }),
   };
 });
 
-
-describe('BasicFruitBandit', () => {
-  it('configuration is currently broken to run these tests', () => {
-    expect(true).toBe(true);
+describe("BasicFruitBandit", () => {
+  beforeEach(() => {
+    SimpleBandit.mockClear();
   });
-  // it('renders without crashing', () => {
-  //   render(<BasicFruitBandit />);
-  // });
 
-  // it('renders fruit probabilities', async () => {
-  //   const { findByText } = render(<BasicFruitBandit />);
-  //   const appleProbability = await findByText(/apple \(33.0%\)/i);
-  //   expect(appleProbability).toBeInTheDocument();
-  // });
+  it("renders without crashing", () => {
+    render(<BasicFruitBandit />);
+  });
 
-  // it('renders fruit recommendation', async () => {
-  //   const { findByText } = render(<BasicFruitBandit />);
-  //   const recommendation = await findByText(/apple/i);
-  //   expect(recommendation).toBeInTheDocument();
-  // });
+  it("renders fruit probabilities", async () => {
+    const { findByText } = render(<BasicFruitBandit />);
+    const appleProbability = screen.getByText(/apple\s*\(33/i);
+    expect(appleProbability).not.toBeNull();
+    const pearProbability = screen.getByText(/pear\s*\(33/i);
+    expect(pearProbability).not.toBeNull();
+    const orangeProbability = screen.getByText(/orange\s*\(33/i);
+    expect(orangeProbability).not.toBeNull();
+  });
 
-  // it('handles accept and reject', async () => {
-  //   const { findByText } = render(<BasicFruitBandit />);
-  //   const acceptButton = await findByText(/accept/i);
-  //   const rejectButton = await findByText(/reject/i);
+  it("handles accept and reject", async () => {
+    render(<BasicFruitBandit />);
+    const acceptButton = screen.getByText("Accept");
+    const rejectButton = screen.getByText("Reject");
 
-  //   fireEvent.click(acceptButton);
-  //   expect(SimpleBandit.mock.instances[0].accept).toHaveBeenCalled();
+    fireEvent.click(acceptButton);
+    await waitFor(() =>
+      expect(SimpleBandit.mock.instances[0].accept).toHaveBeenCalled(),
+    );
 
-  //   fireEvent.click(rejectButton);
-  //   expect(SimpleBandit.mock.instances[0].reject).toHaveBeenCalled();
-  // });
+    fireEvent.click(rejectButton);
+    await waitFor(() =>
+      expect(SimpleBandit.mock.instances[0].reject).toHaveBeenCalled(),
+    );
+  });
 });
